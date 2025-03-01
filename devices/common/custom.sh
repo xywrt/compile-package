49dd78e7b6503d65e92304fe92a26e73be7fc346
Fatih Ka. <xybydy@gmail.com>
2025-03-01 11:09:46 +0300

    wqw

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 shopt -s extglob
-rm -rf feeds/jell/{diy,mt-drivers,shortcut-fe,luci-app-mtwifi,base-files,firewall,dnsmasq}
+rm -rf feeds/kiddin9/{diy,mt-drivers,shortcut-fe,luci-app-mtwifi,base-files,luci-app-package-manager,\
+dnsmasq,firewall*,wifi-scripts,opkg,ppp,curl,luci-app-firewall,\
+nftables,fstools,wireless-regdb,libnftnl}
 
-for ipk in $(find feeds/jell/* -maxdepth 0 -type d);
+curl -sfL https://raw.githubusercontent.com/openwrt/packages/master/lang/golang/golang/Makefile -o feeds/packages/lang/golang/golang/Makefile
+
+for ipk in $(find feeds/kiddin9/* -maxdepth 0 -type d);
 do
 	[[ "$(grep "KernelPackage" "$ipk/Makefile")" && ! "$(grep "BuildPackage" "$ipk/Makefile")" ]] && rm -rf $ipk || true
 done
@@ -13,7 +17,6 @@ rm -Rf feeds/luci/{applications,collections,protocols,themes,libs,docs,contrib}
 rm -Rf feeds/luci/modules/!(luci-base)
 rm -Rf feeds/packages/!(lang|libs|devel|utils|net|multimedia)
 rm -Rf feeds/packages/multimedia/!(gstreamer1|ffmpeg)
-rm -Rf feeds/packages/libs/libcups
 rm -Rf feeds/packages/net/!(mosquitto|curl)
 rm -Rf feeds/base/package/firmware
 rm -Rf feeds/base/package/network/!(services|utils)
@@ -22,22 +25,21 @@ rm -Rf feeds/base/package/system/!(opkg|ubus|uci|ca-certificates)
 rm -Rf feeds/base/package/kernel/!(cryptodev-linux)
 #COMMENT
 
-status=$(curl -H "Authorization: token $WRT_TOKEN" -s "https://api.github.com/repos/kenzok8/small-package/actions/runs" | jq -r '.workflow_runs[0].status')
+status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/xywrt/small-package/actions/runs" | jq -r '.workflow_runs[0].status')
 while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
 echo "wait 5s"
 sleep 5
-status=$(curl -H "Authorization: token $WRT_TOKEN" -s "https://api.github.com/repos/kenzok8/small-package/actions/runs" | jq -r '.workflow_runs[0].status')
+status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/xywrt/small-package/actions/runs" | jq -r '.workflow_runs[0].status')
 done
 
 ./scripts/feeds update -a
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a -p jell -f
+./scripts/feeds install -a -p kiddin9 -f
 ./scripts/feeds install -a
 
-sed -i 's/\(page\|e\)\?.acl_depends.*\?}//' `find package/feeds/jell/luci-*/luasrc/controller/* -name "*.lua"`
-# sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/jell/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
-sed -i 's/Os/O2/g' include/target.mk
+# rm -rf package/feeds/kiddin9/luci-app-quickstart/root/usr/share/luci/menu.d/luci-app-quickstart.json
+
+sed -i 's/\(page\|e\)\?.acl_depends.*\?}//' `find package/feeds/kiddin9/luci-*/luasrc/controller/* -name "*.lua"`
+# sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/kiddin9/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
 
 sed -i \
 	-e "s/+\(luci\|luci-ssl\|uhttpd\)\( \|$\)/\2/" \
@@ -45,19 +47,9 @@ sed -i \
 	-e 's/+python\( \|$\)/+python3/' \
 	-e 's?../../lang?$(TOPDIR)/feeds/packages/lang?' \
 	-e 's,$(STAGING_DIR_HOST)/bin/upx,upx,' \
-	package/feeds/jell/*/Makefile
+	package/feeds/kiddin9/*/Makefile
 
 cp -f devices/common/.config .config
-mv feeds/base feeds/base.bak
-mv feeds/packages feeds/packages.bak
-make defconfig
-rm -Rf tmp
-mv feeds/base.bak feeds/base
-mv feeds/packages.bak feeds/packages
-sed -i 's/CONFIG_ALL=y/CONFIG_ALL=n/' .config
-sed -i '/PACKAGE_kmod-/d' .config
-
-sed -i "/mediaurlbase/d" package/feeds/*/luci-theme*/root/etc/uci-defaults/*
 
 sed -i '/WARNING: Makefile/d' scripts/package-metadata.pl
 
